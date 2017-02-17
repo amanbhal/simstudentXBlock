d7f62b106351e9159a1c995799f7fde33b33b8b4
amanbhal <amandeep.bhal92@gmail.com>
2017-02-17 15:10:23 -0600

    Testing

--- simstudent/simstudent.py
+++ simstudent/simstudent.py
@@ -13,19 +13,17 @@ class simstudentXBlock(XBlock):
     # Fields are defined on the class.  You can access them in your code as
     # self.<fieldname>.
     # TO-DO: change the default href so it is included as a resource in the xblock, not an url
-    href = String(display_name="href",
+    href = String(help="URL for SimStudent HTML rendring",
                   default="http://kona.education.tamu.edu:2401/SimStudentServlet%20%28Working%29/tutor.html?BRD=http%3A%2F%2Fkona.education.tamu.edu%3A2401%2FSimStudentServlet%2Fif_p_or_q_then_r.brd&ARG=-traceOn+-folder+informallogic+-problem+if_p_or_q_then_r+-ssTypeChecker+informallogic.MyFeaturePredicate.valueTypeChecker&CSS=&INFO=&BRMODE=AuthorTimeTutoring&AUTORUN=on&KEYBOARDGROUP=Disabled&BACKDIR=http%3A%2F%2Fkona.education.tamu.edu%3A2401%2FSimStudentServlet%2Fbuild%2Fclasses&BACKENTRY=interaction.ModelTracerBackend&PROBLEM=xxx&DATASET=FlashLoggingTest_xxx&LEVEL1=Unit1&TYPE1=unit&USER=qa-test&GENERATED=on&SOURCE=PACT_CTAT_HTML5&USEOLI=false&SLOG=true&LOGTYPE=None&DISKDIR=.&PORT=4000&REMOTEURL=serv&SKILLS=&VAR1=xxx_xxx&VAL1=xxx&VAR2=xxx_xxx&VAL2=xxx&VAR3=xxx_xxx&VAL3=xxx&VAR4=xxx_xxx&VAL4=xxx&submit=Launch+HTML5+Tutor",
-                  scope=Scope.content,
-                  help="Simstudent file that will be shown in the XBlock")
+                  scope=Scope.content)
 
-    display_name = String(display_name="Display Name",
-                          default="Simstudent File",
-                          scope=Scope.settings,
-                          help="Name of the component in the edxplatform")
-    name = String(display_name="name",
+    display_name = String(help="Name of the component in the edxPlatform",
+			  default="Simstudent File",
+                          scope=Scope.settings)
+    name = String(help="Name of the brd file to run",
                     default="if_p_or_q_then_r.brd",
-                    scope=Scope.content,
-                    help="Name of the brd file to run")
+                    scope=Scope.content)\
+
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -37,10 +35,11 @@ class simstudentXBlock(XBlock):
         The primary view of the simstudentXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("static/html/simstudent.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/simstudent.css"))
-        return frag
+        html = pkg_resources.resource_string(__name__,"static/html/simstudent.html")
+        frag = Fragment(unicode(html).format(self=self))
+	css = pkg_resources.resource_string(__name__,"static/css/simstudent.css")
+        frag.add_css(unicode(css))
+	return frag
 
 
     def studio_view(self, context=None):
@@ -48,9 +47,10 @@ class simstudentXBlock(XBlock):
         The primary view of the simstudentXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("static/html/simstudent_edit.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_javascript(self.resource_string("static/js/src/simstudent_edit.js"))
+        html = pkg_resources.resource_string(__name__,"static/html/simstudent_edit.html")
+        frag = Fragment(unicode(html).format(self=self))
+	js = pkg_resources.resource_string(__name__,"static/js/src/simstudent_edit.js")
+        frag.add_javascript(unicode(js))
         frag.initialize_js('simstudentXBlock')
         return frag
 
@@ -60,8 +60,7 @@ class simstudentXBlock(XBlock):
         An example handler, which increments the data.
         """
         self.href = data['href']
-        self.display_name = data['display_name']
-
+        
         return {'result': 'success'}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
